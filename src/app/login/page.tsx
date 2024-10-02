d989fd9a438bc4881a89cae9dfb00dbd8ef4106a
KAwfang <jitlada.yt@gmail.com>
2024-10-02 22:23:25 +0700

    rename login-form.tsx

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
 import { NavBar } from "@/components/nav-bar";
-import { LoginForm } from "./LoginForm";
+import { LoginForm } from "./login-form";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../api/auth/[...nextauth]/route";
 
