4c1ac7df254c3300c021d8faf554c70720b7e109
KAwfang <jitlada.yt@gmail.com>
2024-10-05 23:41:45 +0700

    create contact component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { NavBar } from "@/components/nav-bar/nav-bar";
 import { getServerSession } from "next-auth";
 import Image from "next/image";
 import { authOptions } from "@/lib/auth";
+import Contact from "@/components/contact";
 
 export default async function Home() {
     const session = await getServerSession(authOptions);
@@ -9,13 +10,13 @@ export default async function Home() {
         <div>
             <NavBar session={session} />
             <Image
-                src="/images/home.png"
+                src="/images/banner.svg"
                 alt="Poster"
                 layout="responsive"
                 width={1920}
                 height={818.67}
             />
-
+            <Contact />
         </div>
     );
 }
