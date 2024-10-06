14269e0cd1d4102437c226a882d410b0ebaffabe
Pimmada <pimmadapng@gmail.com>
2024-10-07 01:21:34 +0700

    Add order page

---
+++ src/app/order/page.tsx
@@ -0,0 +1,21 @@
+import { NavBar } from "@/components/nav-bar/nav-bar";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/lib/auth";
+import OrderForm from "@/components/order-form";
+
+
+export default async function OrderPage() {
+    const session = await getServerSession(authOptions);
+
+    return (
+        <div style={{ backgroundColor: "#FFF7F9", minHeight: "100vh", paddingBottom: "60px" }}>
+            <NavBar session={session} />
+            <div className="flex flex-col items-center gap-12">
+                <h1 className="text-center font-bold text-4xl h-full">
+                    Order
+                </h1>
+                <OrderForm/>
+            </div>
+        </div>
+    );
+}
