0d5d16d6f53d0e0086f65eda525f4bedb4a60447
Pimmada <pimmadapng@gmail.com>
2024-10-07 17:27:14 +0700

    Edit svg elements

--- src/components/register-complete.tsx
+++ src/components/register-complete.tsx
@@ -10,9 +10,9 @@ export function RegisterComplete() {
           width="120" height="120" 
           viewBox="0 0 24 24" 
           fill="none" stroke="currentColor" 
-          stroke-width="1" 
-          stroke-linecap="round" 
-          stroke-linejoin="round" 
+          strokeWidth="1" 
+          strokeLinecap="round" 
+          strokeLinejoin="round" 
           className="lucide lucide-circle-check-big">
           <path d="M21.801 10A10 10 0 1 1 17 3.335"/>
           <path d="m9 11 3 3L22 4"/>
