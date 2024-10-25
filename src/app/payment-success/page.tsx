4e97e7ab24e0754145bf8212a16ac1c0c727cc51
Pimmada <pimmadapng@gmail.com>
2024-10-26 02:58:28 +0700

    Fix payment-success and register-complete

--- src/app/payment-success/page.tsx
+++ src/app/payment-success/page.tsx
@@ -2,14 +2,9 @@ import { NavBar } from "@/components/nav-bar/nav-bar";
 import { getServerSession } from "next-auth";
 import { authOptions } from '@/lib/auth';
 import { PaymentSuccess } from "@/components/payment-success";
-import { redirect } from 'next/navigation';
 
 export default async function RegisterPage() {
     const session = await getServerSession(authOptions);
-    
-    if (session) {
-        redirect('/');
-    }
 
     return (
         <div style={{ backgroundColor: "#FFF7F9", minHeight: "100vh", paddingBottom: "60px" }}>
--- src/app/register-complete/page.tsx
+++ src/app/register-complete/page.tsx
@@ -2,20 +2,15 @@ import { NavBar } from "@/components/nav-bar/nav-bar";
 import { getServerSession } from "next-auth";
 import { authOptions } from '@/lib/auth';
 import { RegisterComplete } from "@/components/register-complete";
-import { redirect } from 'next/navigation';
 
-export default async function RegisterPage() {
+export default async function RegisterCompletePage() {
     const session = await getServerSession(authOptions);
-    
-    if (session) {
-        redirect('/');
-    }
 
     return (
         <div style={{ backgroundColor: "#FFF7F9", minHeight: "100vh", paddingBottom: "60px" }}>
             <NavBar session={session} />
             <div className="flex flex-col items-center gap-12 pt-[100px]">
-                <RegisterComplete/>
+                <RegisterComplete />
             </div>
         </div>
     );
