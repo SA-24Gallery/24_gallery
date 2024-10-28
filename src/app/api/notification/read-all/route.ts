11df0400f9ded910d0d48bd65e84f6b59fff8395
KAwfang <jitlada.yt@gmail.com>
2024-10-29 02:24:24 +0700

    fix notification

--- src/app/api/notification/read-all/route.ts
+++ src/app/api/notification/read-all/route.ts
@@ -1,14 +1,32 @@
 import { query } from '@/lib/db';
 import { NextResponse } from 'next/server';
+import {getServerSession} from "next-auth";
+import {authOptions} from "@/lib/auth";
 
 export async function PUT() {
     try {
-        const sql = 'UPDATE NOTIFIED_MSG SET Is_read = 1 WHERE Is_read = 0';
-        await query(sql);
+        const session = await getServerSession(authOptions);
+        if (!session || !session.user) {
+            return NextResponse.json({ error: "Not authenticated" });
+        }
+        const email = session?.user?.email
+
+        if (!email) {
+            return NextResponse.json(
+                { message: 'Email is required' },
+                { status: 400 }
+            );
+        }
+
+        const sql = 'UPDATE NOTIFIED_MSG SET Is_read = 1 WHERE Is_read = 0 AND Email = ?';
+        await query(sql, [email]);
 
         return NextResponse.json({ message: 'All notifications marked as read' });
     } catch (error) {
         console.error('Failed to mark notifications as read:', error);
-        return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
+        return NextResponse.json(
+            { message: 'Internal Server Error' },
+            { status: 500 }
+        );
     }
 }
